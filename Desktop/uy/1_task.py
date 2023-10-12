7f7bf809ec75ee0e87f40028903565375e77c0e9
gamesupro01@gmail.com <gamesupro01@gmail.com>
2023-10-12 13:54:54 +0500

    uy

---
+++ Desktop/uy/1_task.py
@@ -0,0 +1,13 @@
+class Airoport:
+    def __init__(self, nom, ochilish_yili) -> None:
+        self.nom = nom
+        self.ochilish_yil = ochilish_yili
+
+    def get_info(self) -> str:
+        print(f"""
+Airoport nomi: {self.nom}
+Airoport ochilgan yili: {self.ochilish_yil}
+""")
+        
+a1 = Airoport("Air №1", 1997)
+a1.get_info()
